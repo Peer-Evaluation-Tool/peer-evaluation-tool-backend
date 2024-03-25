15772845e5d7754b34e4e7582d3833246235e7dc
James Nogueira <j.l.nogueira@tcu.edu>
2024-03-25 09:15:35 -0500

    This is the first commit. ;)

---
+++ src/test/java/edu/tcu/cs/peerevalutationtool/PeerEvalutationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.peerevalutationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerEvalutationToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
